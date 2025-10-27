63b4500b3a92b7f3dc08f948d0d489ecbd160e95
Jack41784090 <tszmingjack@gmail.com>
2025-10-27 14:29:59 -0700

    Refactor StatusEffectEventBus to use proper signals and emission

--- src/StatusEffectEventBus.gd
+++ src/StatusEffectEventBus.gd
@@ -1,45 +1,45 @@
 extends Node2D
 
-signal _hello_world(val)
-signal _target_took_damage(val)
+signal HelloWorld(val)
+signal TargetTookDamage(val)
+signal OnBasicAttackHit(attacker, target, damage)
 
 enum Signals {
 	HelloWorld,
 	
-	TargetTookDamage
+	TargetTookDamage,
+	OnBasicAttackHit
 }
 
-const _signals: Dictionary = {
-	Signals.HelloWorld: "_hello_world",
-	Signals.TargetTookDamage: "_target_took_damage"
-}
-
-func _get_signal_name(signal_enum: Signals) -> String:
+func _get_signal(signal_enum: Signals) -> Signal:
 	match signal_enum:
-		Signals.HelloWorld: return "HelloWorld"
-		Signals.TargetTookDamage: return "TargetTookDamage"
-		_: return "Unknown_%d" % signal_enum
+		Signals.HelloWorld: return HelloWorld
+		Signals.TargetTookDamage: return TargetTookDamage
+		Signals.OnBasicAttackHit: return OnBasicAttackHit
+		_: assert(false, "Unknown signal enum: %s" % signal_enum); return HelloWorld  # Fallback
 
-func emitSignal(nameOFSignal: Signals, ...args: Array):
-	print("  [EventBus] Emitting signal: %s with %d args" % [_get_signal_name(nameOFSignal), args.size()])
-	var signal_name = _signals.get(nameOFSignal)
-	
+func EmitSignal(nameOfSignal: Signals, ...args: Array):
+	var _signal = _get_signal(nameOfSignal)
+	print("  [EventBus] Emitting signal: %s with %d args" % [_signal, args.size()])
+	_signal.emit(args)
 	# Use emit_signal with proper argument spreading
-	if args.size() == 0:
-		return self.emit_signal(signal_name)
-	elif args.size() == 1:
-		return self.emit_signal(signal_name, args[0])
-	else:
-		# For multiple args, use callv to spread them
-		return self.callv("emit_signal", [signal_name] + args)
+	# if args.size() == 0:
+	# 	return _signal.emit_signal()
+	# elif args.size() == 1:
+	# 	return _signal.emit_signal(args[0])
+	# else:
+	# 	# For multiple args, use callv to spread them
+	# 	return _signal.callv([_signal] + args)
 
 func Connect(nameOfSignal: Signals, function):
-	print("  [EventBus] Connected: %s" % _get_signal_name(nameOfSignal))
-	return self.connect(_signals.get(nameOfSignal), function)
+	var _signal = _get_signal(nameOfSignal)
+	print("  [EventBus] Connected: %s" % _signal)
+	return _signal.connect(function)
 
 func Disconnect(nameOfSignal: Signals, function):
-	print("  [EventBus] Disconnected: %s" % _get_signal_name(nameOfSignal))
-	self.disconnect(_signals.get(nameOfSignal), function)
+	var _signal = _get_signal(nameOfSignal)
+	print("  [EventBus] Disconnected: %s" % _signal)
+	_signal.disconnect(function)
 
 # #button.gd
 # var worldName := "Earth"
