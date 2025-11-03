1ee12f3293347b47c854fb946525da9767be4436
Jack41784090 <tszmingjack@gmail.com>
2025-11-03 14:40:23 -0800

    Remove unused demo files

--- src/demos/strategy-demo-instant.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var scenario: GameScenario = preload("res://resources/scenarios/test scenario/t-scene.tres")
-
-func _ready() -> void:
-	var activity = DrillActivity.new()
-	var turn = scenario.execute_turn(activity)
-	print(turn)
-	pass
