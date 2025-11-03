2631d1e458d2e5ed3e7f8c36f39af0a82a842c12
Jack41784090 <tszmingjack@gmail.com>
2025-11-03 14:51:41 -0800

    Add LogicFactory for centralized logic resource loading

---
+++ src/squad_battle/entity/logic/_factory.gd
@@ -0,0 +1,16 @@
+class_name LogicFactory
+
+enum LogicAvailable {
+	Frontline
+}
+
+static var pathlib = {
+	"Frontline": "res://resources/combat/logic/test-frontline.tres"
+}
+static var _cached_key = LogicAvailable.keys()
+static func get_logic(_logic: LogicAvailable):
+	var path = pathlib.get(_cached_key[_logic]);
+	var logic = load(path)
+	assert(logic != null, "Failed to load logic from path: %s" % path)
+	assert(logic is SimplifiedLogicConfig, "Path %s loaded wrong type; got %s instead of SimplifiedLogicConfig" % [path, logic.get_class()])
+	return logic
