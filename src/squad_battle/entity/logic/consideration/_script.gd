9e5c8feb77e8416afa4b312f23254d4ef8a79112
Jack41784090 <tszmingjack@gmail.com>
2025-11-05 16:35:29 -0800

    Simplify Consideration class to focus on glances and remove old sub-consideration logic

--- src/squad_battle/entity/logic/consideration/_script.gd
+++ src/squad_battle/entity/logic/consideration/_script.gd
@@ -1,34 +1,31 @@
 class_name Consideration extends Resource
 
+@export var name: String = ""
 @export var weight: float = 1.0
-@export var op: CsdrTypes.OP
-
-@export var subcsdr: Array[Consideration]
+@export var op: CsdrTypes.OP = CsdrTypes.OP.ADD
 @export var glances: Array[Glance] = []
 @export var entity_limiter: String = "all"
 @export var skill: Skill = null
+@export var average_score_between_glances: bool = false
 
 func score(entity, situation, context) -> float:
-	var result: float
-	
-	if glances.size() > 0:
-		result = _score_with_glances(entity, situation, context)
-	else:
-		result = _score_with_subcsdr(entity, situation, context)
-	
-	return result
+	return _score_with_glances(entity, situation, context)
 
-func _score_with_glances(entity, situation, context) -> float:
+func _score_with_glances(entity, situation, _context) -> float:
 	var entities_to_evaluate = _get_entities_to_evaluate(entity, situation)
 	
 	if entities_to_evaluate.is_empty():
+		print("  [Consideration] No entities to evaluate, returning 0.0")
 		return 0.0
+
+	if glances.is_empty():
+		print("  [Consideration] No glances to evaluate, returning the default weight: %s", weight)
+		return weight
 	
 	var glance_scores: Array[float] = []
 	for entity_to_check in entities_to_evaluate:
-		var entity_score = 0.0
-		for glance in glances:
-			entity_score += glance.evaluate(entity_to_check)
+		var entity_score = glances.reduce(func(acc: float, glance: Glance): return acc + glance.evaluate(entity_to_check), 0.0)
+		if average_score_between_glances: entity_score = entity_score / glances.size()
 		glance_scores.append(entity_score)
 	
 	var result: float
@@ -38,10 +35,7 @@ func _score_with_glances(entity, situation, context) -> float:
 		CsdrTypes.OP.MUL:
 			result = glance_scores.reduce(func(acc: float, val: float): return acc * val, 1.0)
 		CsdrTypes.OP.AVG:
-			if glance_scores.size() > 0:
-				result = glance_scores.reduce(func(acc: float, val: float): return acc + val, 0.0) / glance_scores.size()
-			else:
-				result = 0.0
+			result = glance_scores.reduce(func(acc: float, val: float): return acc + val, 0.0) / entities_to_evaluate.size()
 		CsdrTypes.OP.RDC:
 			result = glance_scores.reduce(func(acc: float, val: float): return acc - val, 0.0)
 		_:
@@ -53,32 +47,6 @@ func _score_with_glances(entity, situation, context) -> float:
 		CsdrTypes.OP.keys()[op], entities_to_evaluate.size(), weight, result])
 	return result
 
-func _score_with_subcsdr(entity, situation, context) -> float:
-	var result: float
-	match op:
-		CsdrTypes.OP.ADD:
-			var sum: float = subcsdr.reduce(func(acc: float, csdr: Consideration): return acc + csdr.score(entity, situation, context), 0.0)
-			result = sum * weight
-			print("  [Consideration.ADD] sum=%.2f, weight=%.2f → %.2f" % [sum, weight, result])
-		CsdrTypes.OP.RDC:
-			var sum: float = subcsdr.reduce(func(acc: float, csdr: Consideration): return acc - csdr.score(entity, situation, context), 0.0)
-			result = sum * weight
-			print("  [Consideration.RDC] sum=%.2f, weight=%.2f → %.2f" % [sum, weight, result])
-		CsdrTypes.OP.MUL:
-			var sum: float = subcsdr.reduce(func(acc: float, csdr: Consideration): return acc * csdr.score(entity, situation, context), 1.0)
-			result = sum * weight
-			print("  [Consideration.MUL] sum=%.2f, weight=%.2f → %.2f" % [sum, weight, result])
-		CsdrTypes.OP.AVG:
-			if subcsdr.size() > 0:
-				var sum: float = subcsdr.reduce(func(acc: float, csdr: Consideration): return acc + csdr.score(entity, situation, context), 0.0)
-				result = (sum / subcsdr.size()) * weight
-				print("  [Consideration.AVG] avg=%.2f, weight=%.2f → %.2f" % [sum / subcsdr.size(), weight, result])
-			else:
-				result = 0.0
-		_:
-			assert(false, "Unimplemented Operation in Consideration used")
-			result = 0.0
-	return result
 
 func _get_entities_to_evaluate(entity, situation) -> Array:
 	var entities: Array = []
@@ -86,33 +54,29 @@ func _get_entities_to_evaluate(entity, situation) -> Array:
 		"self":
 			entities.append(entity)
 		"allies":
-			var unwrapped = situation.unwrap()
-			if unwrapped.has("our_squad"):
-				var our_squad = unwrapped["our_squad"]
-				for location in our_squad.values():
-					if location is Array:
-						entities.append_array(location)
+			var our_squad = situation.get_our_squad()
+			for location in our_squad.values():
+				if location is Array:
+					entities.append_array(location)	
 		"enemies":
-			var unwrapped = situation.unwrap()
-			if unwrapped.has("enemy_squad"):
-				var enemy_squad = unwrapped["enemy_squad"]
-				for location in enemy_squad.values():
-					if location is Array:
-						entities.append_array(location)
+			var enemy_squad = situation.get_enemy_squad()
+			for location in enemy_squad.values():
+				if location is Array:
+					entities.append_array(location)
 		"all":
-			var unwrapped = situation.unwrap()
-			if unwrapped.has("our_squad"):
-				var our_squad = unwrapped["our_squad"]
-				for location in our_squad.values():
-					if location is Array:
-						entities.append_array(location)
-			if unwrapped.has("enemy_squad"):
-				var enemy_squad = unwrapped["enemy_squad"]
-				for location in enemy_squad.values():
-					if location is Array:
-						entities.append_array(location)
+			var our_squad = situation.get_our_squad()
+			# print("Our squad: %s" % our_squad)
+			for location in our_squad.values():
+				if location is Array:
+					entities.append_array(location)
+			var enemy_squad = situation.get_enemy_squad()
+			# print("Enemy squad: %s" % enemy_squad)
+			for location in enemy_squad.values():
+				if location is Array:
+					entities.append_array(location)
 		_:
-			entities.append(entity)
+			assert(false, "Unimplemented entity limiter: %s" % entity_limiter)
 	
+	# print(entities)
 	return entities
 
