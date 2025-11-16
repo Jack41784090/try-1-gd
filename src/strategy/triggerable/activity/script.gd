68d6d85dc9c080f492e3412ae88d40898d664dbf
Jack41784090 <tszmingjack@gmail.com>
2025-11-16 13:59:12 -0800

    Refactor activity script exports and logic

--- src/strategy/triggerable/activity/script.gd
+++ src/strategy/triggerable/activity/script.gd
@@ -1,12 +1,13 @@
 # activity.gd
 class_name Activity extends Triggerable
 
+@export var result: ActivityResult;
 @export var activity_type: StrategyTypes.ActivityType = StrategyTypes.ActivityType.CUSTOM
 @export var time_cost: int = 1
-@export var resource_costs: Dictionary[StrategyTypes.SquadProperty, float] = {} # {"food": 5, "money": 10.0}
-@export var location_requirements: Array[StrategyTypes.LocationType] = []
-@export var base_effects: Dictionary[StrategyTypes.SquadProperty, float] = {} # Squad stat modifications
-@export var event_chain_path: String = ""
+@export var resource_costs: Dictionary = {} # {"food": 5, "money": 10.0}
+@export var base_effects: Dictionary = {} # Squad stat modifications
+@export var location_requirements: Array = [] # Array of LocationType enums
+# @export var event_chain_path: String = ""
 
 # Legacy cost fields for compatibility
 var food_cost: int:
@@ -23,8 +24,8 @@ var travel_tools_cost: int:
 @export var custom_script: Script = null
 
 func can_execute(squad: StrategicSquad, location: Location) -> bool:
-	if location_requirements.size() > 0 and not location.type in location_requirements:
-		return false
+	# if location_requirements.size() > 0 and not location.type in location_requirements:
+	# 	return false
 	
 	if food_cost > 0 and squad.food < food_cost:
 		return false
@@ -38,8 +39,8 @@ func can_execute(squad: StrategicSquad, location: Location) -> bool:
 	return true
 
 func get_cannot_execute_reason(squad: StrategicSquad, location: Location) -> String:
-	if location_requirements.size() > 0 and not location.type in location_requirements:
-		return "This activity is not available at this location type."
+	# if location_requirements.size() > 0 and not location.type in location_requirements:
+	# 	return "This activity is not available at this location type."
 	
 	if food_cost > 0 and squad.food < food_cost:
 		return "Not enough food. Need %d, have %d." % [food_cost, squad.food]
