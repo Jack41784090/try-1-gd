46fb7cc58ea62a4359ff78e2b656e61a676ff42b
Jack41784090 <tszmingjack@gmail.com>
2025-11-16 17:48:35 -0800

    Add _to_string methods to result classes

--- src/strategy/generic-result.gd
+++ src/strategy/generic-result.gd
@@ -6,6 +6,14 @@ class_name GenericResult extends Resource
 @export var requires_async: bool
 # @export var triggered_event_ids: Array[String] = []
 
+func _to_string() -> String:
+    return "GenericResult(squad_stat_changes=%s, world_stat_changes=%s, event_chain_path=%s, requires_async=%s)" % [
+        squad_stat_changes,
+        world_stat_changes,
+        event_chain_path,
+        requires_async
+    ]
+
 func _init(_config: Dictionary = {}) -> void:
     for key in _config.keys():
         if self.get(key) != null:
--- src/strategy/triggerable/activity/result.gd
+++ src/strategy/triggerable/activity/result.gd
@@ -2,6 +2,9 @@ class_name ActivityResult extends GenericResult
 
 @export var location_changed: String = ""
 
+func _to_string() -> String:
+	return "ActivityResult(location_changed=%s, %s)" % [location_changed, super._to_string()]
+
 func _init(config: Dictionary = {}) -> void:
 	super._init(config)
 	location_changed = config.get("location_changed", "")
