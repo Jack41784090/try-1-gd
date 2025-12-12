a1c53ff1d13ed75e84cceac9d717eda431a32deb
Jack41784090 <tszmingjack@gmail.com>
2025-12-12 00:49:03 -0800

    Update Activity system for combat triggers and Force March mechanics

--- src/strategy/triggerable/activity/result.gd
+++ src/strategy/triggerable/activity/result.gd
@@ -1,9 +1,12 @@
 class_name ActivityResult extends GenericResult
 
 @export var location_changed: String = ""
+@export var requires_combat: bool = false
+@export var combat_target_squad_id: String = ""
+@export var clues_left: int = 0
 
 func _to_string() -> String:
-	return "ActivityResult(location_changed=%s, %s)" % [location_changed, super._to_string()]
+	return "ActivityResult(location_changed=%s, requires_combat=%s, combat_target=%s, %s)" % [location_changed, requires_combat, combat_target_squad_id, super._to_string()]
 
 func _init(config: Dictionary = {}) -> void:
 	print("config", config)
--- src/strategy/triggerable/activity/script.gd
+++ src/strategy/triggerable/activity/script.gd
@@ -4,8 +4,10 @@ class_name Activity extends Triggerable
 @export var result: ActivityResult;
 @export var activity_type: StrategyTypes.ActivityType = StrategyTypes.ActivityType.CUSTOM
 @export var time_cost: int = 1
-# @export var event_chain_path: String = ""
-
+@export var destination_id: String = ""
+@export var min_stability_to_block_attack: float = 80.0
+@export var force_march_supply_multiplier: float = 2.0
+@export var force_march_clue_multiplier: int = 2
 
 # Optional custom logic override
 @export var custom_script: Script = null
@@ -14,7 +16,18 @@ func _to_string() -> String:
 	return "Activity: %s (Type: %s, Time Cost: %d, %s)" % [trigger_name, StrategyTypes.ActivityType.keys()[activity_type], time_cost, super()]
 
 func can_execute(squad: StrategicSquad, location: Location) -> bool:
-	return true
+	match activity_type:
+		StrategyTypes.ActivityType.ATTACK:
+			return location.stability < min_stability_to_block_attack
+		StrategyTypes.ActivityType.FORCE_MARCH:
+			if destination_id.is_empty():
+				return false
+			if not location.is_connected_to(destination_id):
+				return false
+			var food_cost = int(1 * force_march_supply_multiplier)
+			return squad.food >= food_cost
+		_:
+			return true
 
 func trigger(squad: StrategicSquad, world: World) -> ActivityResult:
 	# execution_started.emit()
@@ -31,11 +44,50 @@ func execute(squad: StrategicSquad, world: World) -> ActivityResult:
 	
 	return _execute_generic(squad, world)
 
-func _execute_generic(_squad: StrategicSquad, _world: World) -> ActivityResult:
-	# if not result:
-	# 	result = ActivityResult.new({})
+func _execute_generic(squad: StrategicSquad, world: World) -> ActivityResult:
 	assert(result)
-	# if not event_chain_path.is_empty():
-	# 	result.event_chain_path = event_chain_path
-	# 	result.requires_async = true
+	
+	match activity_type:
+		StrategyTypes.ActivityType.ATTACK:
+			return _execute_attack(squad, world)
+		StrategyTypes.ActivityType.FORCE_MARCH:
+			return _execute_force_march(squad, world)
+		_:
+			return result
+
+func _execute_attack(squad: StrategicSquad, world: World) -> ActivityResult:
+	var enemies_here = world.get_squads_at_location(squad.current_location_id)
+	
+	if enemies_here.is_empty():
+		result.modify_squad_stat(StrategyTypes.SquadProperty.MORALE, -5.0)
+		return result
+	
+	var target_enemy = enemies_here[0]
+	result.requires_combat = true
+	result.combat_target_squad_id = target_enemy.squad_id
+	result.requires_async = true
+	return result
+
+func _execute_force_march(squad: StrategicSquad, world: World) -> ActivityResult:
+	var food_cost = int(1 * force_march_supply_multiplier)
+	squad.consume_food(food_cost)
+	
+	var old_location = world.get_location_by_id(squad.current_location_id)
+	
+	if old_location:
+		for i in range(force_march_clue_multiplier):
+			var clues = squad.attempt_stealth_at_location(old_location, destination_id, world.turn_count)
+			result.clues_left += clues.size()
+	
+	squad.set_location(destination_id)
+	result.location_changed = destination_id
+	result.modify_squad_stat(StrategyTypes.SquadProperty.MORALE, -10.0)
+	
+	var enemies_at_destination = world.get_squads_at_location(destination_id)
+	if not enemies_at_destination.is_empty():
+		var target_enemy = enemies_at_destination[0]
+		result.requires_combat = true
+		result.combat_target_squad_id = target_enemy.squad_id
+		result.requires_async = true
+	
 	return result
