a7cd44ad4ca2bf88c078ec2ada51fe5cc00dc34c
Jack41784090 <tszmingjack@gmail.com>
2026-01-03 16:28:25 +0800

    Add new scripts for Manage Squad feature

---
+++ src/strategy/ui/manage_squad.gd
@@ -0,0 +1,39 @@
+extends Control
+class_name ManageSquadScreen
+
+signal closed
+
+@onready var title_label: Label = $OverlayPanel/MarginContainer/VBoxContainer/TitleLabel
+@onready var warriors_container: VBoxContainer = $OverlayPanel/MarginContainer/VBoxContainer/ScrollContainer/WarriorsContainer
+@onready var close_button: Button = $OverlayPanel/MarginContainer/VBoxContainer/CloseButton
+
+const WARRIOR_ITEM_SCENE = preload("res://scenes/warrior_item.tscn")
+
+var current_squad: StrategicSquad = null
+
+func _ready() -> void:
+	close_button.pressed.connect(_on_close_pressed)
+
+func show_squad(squad: StrategicSquad) -> void:
+	current_squad = squad
+	visible = true
+	_populate_warrior_list()
+
+func hide_screen() -> void:
+	visible = false
+	current_squad = null
+
+func _populate_warrior_list() -> void:
+	for child in warriors_container.get_children():
+		child.queue_free()
+	
+	title_label.text = "Squad: %s" % current_squad.squad_name
+	
+	for warrior in current_squad.warriors:
+		var warrior_item = WARRIOR_ITEM_SCENE.instantiate()
+		warrior_item.setup_warrior(warrior)
+		warriors_container.add_child(warrior_item)
+
+func _on_close_pressed() -> void:
+	hide_screen()
+	closed.emit()
