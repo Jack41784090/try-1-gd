def3912804b57d963afc45b475124cf606c8b658
Jack41784090 <tszmingjack@gmail.com>
2026-01-18 17:46:06 +0800

    Add utility methods to TriggerChain class

--- src/strategy/triggerable/chain.gd
+++ src/strategy/triggerable/chain.gd
@@ -1,3 +1,15 @@
 class_name  TriggerChain extends Resource
 @export var another_trigger: Triggerable
 @export var chance: float = 1.0
+# @export var conditions : Array[TriggerCondition] = []
+
+func _to_string() -> String:
+	return "TriggerChain(Another Trigger: %s, Chance: %.2f)" % [
+		another_trigger if "another_trigger" else "None",
+		chance
+	]
+
+func _init(_at: Triggerable = null, _c: float = 1.0) -> void:
+	if _at != null:
+		another_trigger = _at
+		chance = _c
