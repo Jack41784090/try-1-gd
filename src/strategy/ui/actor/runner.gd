ce13925689b339b56b00f291ad96a55fe0f0df76
Jack41784090 <tszmingjack@gmail.com>
2026-01-31 15:51:57 +0800

    Implement travel progress and towards location tracking in actor runner

--- src/strategy/ui/actor/runner.gd
+++ src/strategy/ui/actor/runner.gd
@@ -7,15 +7,43 @@ var locations:
 	get:
 		return data.world.travel_graph.locations
 
+var travel_progress:
+	get:
+		if walking_towards == null or walking_towards["location"] == null:
+			return 0
+		return walking_towards["progress"]
+	set(_tp):
+		assert(_tp is float or _tp is int)
+		if walking_towards == null or walking_towards["location"] == null:
+			return
+		walking_towards["progress"] = _tp
+
 var walking_towards: Variant:
+	get:
+		if walking_towards == null:
+			walking_towards = null
+		return walking_towards
 	set(_cl):
-		if _cl is String: walking_towards = data.scenario.travel_graph.get_location(_cl)
-		elif _cl is Location: walking_towards = _cl
-		else: assert(false)
+		if !_cl:
+			walking_towards = {"location": null, "progress": 0}
+			return
+			
+		assert(_cl is String or _cl is Location)
+		var new_loc: Location = data.scenario.world.travel_graph.get_location(_cl) if _cl is String else _cl
+		if not walking_towards["location"]:
+			walking_towards = {"location": new_loc, "progress": 0}
+		elif new_loc.location_id == walking_towards["location"].location_id:
+			walking_towards = {"location": new_loc, "progress": walking_towards["progress"] + 1}
+		else:
+			walking_towards = {"location": new_loc, "progress": 0}
 
 var current_location: Variant:
+	get:
+		if current_location == null:
+			current_location = data.scenario.starting_location_id
+		return current_location
 	set(_cl):
-		if _cl is String: current_location = data.scenario.travel_graph.get_location(_cl)
+		if _cl is String: current_location = data.scenario.world.travel_graph.get_location(_cl)
 		elif _cl is Location: current_location = _cl
 		else: assert(false)
 
@@ -24,12 +52,21 @@ func setup(_loaded_scenario, context = {}):
 	data.setup(_loaded_scenario, context)
 
 func embark_new_journey(towards: Location):
-	var squad = data.player_squad
+	var _squad = data.player_squad
 	current_location = data.scenario.starting_location_id
-	data.world.travel_graph.walking_towards = towards
-	
+	walking_towards = towards
 
-func get_all_reachable_locations(from_id: String, max_hops: int = -1) -> Array[String]:
+func get_distance(current_id, location_id):
+	return data.scenario.world.travel_graph.calculate_travel_time_from(current_id, location_id)
+
+func get_location_by_id(location_id):
+	return data.scenario.world.get_location_by_id(location_id)
+
+func get_all_reachable_locations(from_id: Variant, max_hops: int = -1) -> Array[String]:
+	assert(from_id is String or from_id is Location)
+	if from_id is Location:
+		from_id = from_id.location_id
+	
 	if not locations.has(from_id):
 		return []
 	
@@ -48,10 +85,11 @@ func get_all_reachable_locations(from_id: String, max_hops: int = -1) -> Array[S
 		if max_hops >= 0 and current_depth >= max_hops:
 			continue
 		
-		if not current_location:
+		var current_loc = locations.get(current_id)
+		if not current_loc:
 			continue
 		
-		for neighbor_id in current_location.connected_location_ids:
+		for neighbor_id in current_loc.connected_location_ids:
 			if not visited.has(neighbor_id):
 				visited[neighbor_id] = true
 				queue.append([neighbor_id, current_depth + 1])
