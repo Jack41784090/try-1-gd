631c28d09e84ffb8e04ca5a1a58d329b1714ea70
ikcha-odoo <ikcha@odoo.com>
2026-02-28 19:07:58 +0800

    Add core strategy types and enums

--- src/strategy/types.gd
+++ src/strategy/types.gd
@@ -1,4 +1,5 @@
 extends RefCounted
+
 class_name StrategyTypes
 
 enum LocationType {
@@ -6,7 +7,7 @@ enum LocationType {
 	TOWN,
 	VILLAGE,
 	FORT,
-	ROAD
+	ROAD,
 }
 
 enum Religion {
@@ -17,7 +18,7 @@ enum Religion {
 	PROTESTANT,
 	PAGAN,
 	BUDDHIST,
-	TENGRIST
+	TENGRIST,
 }
 
 enum ActivityType {
@@ -47,7 +48,7 @@ enum TriggerWhen {
 
 enum LogicalOperator {
 	AND,
-	OR
+	OR,
 }
 
 enum GlobalModifier {
@@ -56,7 +57,7 @@ enum GlobalModifier {
 	WOOD,
 	WATER,
 	FIRE,
-	EARTH
+	EARTH,
 }
 
 enum WarriorAttribute {
@@ -64,24 +65,28 @@ enum WarriorAttribute {
 	SURVIVAL,
 	PERCEPTION,
 	LEADERSHIP,
-	STEALTH
+	STEALTH,
 }
+
 
 class EventChoice:
 	var choice_id: String
 	var choice_text: String
 	var conditions: Array = []
-	var effects: Dictionary = {}
-	
+	var effects: Dictionary = { }
+
+
 	func _init(p_id: String = "", p_text: String = "") -> void:
 		choice_id = p_id
 		choice_text = p_text
-	
+
+
 	func is_available(context: Dictionary) -> bool:
 		for condition in conditions:
 			if not condition.evaluate(context):
 				return false
 		return true
+
 
 class TriggerContext:
 	var squad: Resource
@@ -90,11 +95,13 @@ class TriggerContext:
 	var location: Resource
 	var turn: int = 0
 	var completed_missions: Array[String] = []
-	
+
+
 	func _init(p_squad: Resource = null, p_world: Resource = null) -> void:
 		squad = p_squad
 		world = p_world
-	
+
+
 	func to_dict() -> Dictionary:
 		return {
 			"squad": squad,
@@ -102,7 +109,7 @@ class TriggerContext:
 			"activity": activity,
 			"location": location,
 			"turn": turn,
-			"completed_missions": completed_missions
+			"completed_missions": completed_missions,
 		}
 
 
@@ -114,28 +121,52 @@ enum SquadProperty {
 	FUEL_SUPPLIES,
 	MORALE,
 	DISCIPLINE,
-	EXPERIENCE
+	EXPERIENCE,
 }
 
 enum ItemType {
-	SUPPLY
+	SUPPLY,
 }
 
 enum ContactState {
 	NONE,
 	SUSPECTED,
 	TRACKED,
-	LOCKED
+	LOCKED,
 }
 
 enum EngagementType {
 	AMBUSH,
 	SET_PIECE,
 	MEETING,
-	AVOIDANCE
+	AVOIDANCE,
 }
 
 enum EngagementStance {
 	ALWAYS_ENGAGE,
-	ENGAGE_WHEN_CONFIRMED
+	ENGAGE_WHEN_CONFIRMED,
 }
+
+enum SocialClass {
+	PEASANT,
+	SOLDIER,
+	MERCHANT,
+	NOBLE,
+	CLERGY,
+}
+
+
+static func get_social_class_food_demand(social_class: SocialClass) -> float:
+	match social_class:
+		SocialClass.PEASANT:
+			return 0.5
+		SocialClass.SOLDIER:
+			return 1.0
+		SocialClass.MERCHANT:
+			return 0.75
+		SocialClass.NOBLE:
+			return 1.5
+		SocialClass.CLERGY:
+			return 0.75
+		_:
+			return 1.0
