e15730e198c2eb5aefa055c00a1c702a5f0a613a
Jack41784090 <tszmingjack@gmail.com>
2026-08-24 19:53:23 +0800

    feat(strategy): add default values to TownConnection._init

--- src/strategy/core/town/connection.gd
+++ src/strategy/core/town/connection.gd
@@ -5,7 +5,7 @@ extends Resource
 @export var to_location_id: String = ""
 @export var distance_km: float = 10.0
 
-func _init(_from, _to, _dist) -> void:
+func _init(_from: String = "", _to: String = "", _dist: float = 10.0) -> void:
     from_location_id = _from
     to_location_id = _to
     distance_km = _dist
